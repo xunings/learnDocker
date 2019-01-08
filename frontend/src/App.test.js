1f38e0b3eb6d079f31687d45a9fb961a6fa59722
xunings <arc.neco@gmail.com>
2019-01-08 21:05:07 +0800

    Add a tests service.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -8,5 +8,11 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 
 
